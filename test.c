abd9b57c9c548ef2e96941e8b42a72ae891904d0
dhaven <david.haven.dh@gmail.com>
2016-05-14 12:25:13 +0900

    test file

--- test.c
+++ test.c
@@ -9,22 +9,16 @@
 double *C;
 int x = 5;
 int y = 5;
-void initialize(double *tab){
-	tab[0] = 0.0;
-	tab[1] = 1.0;
+void initialize(double *tab[width]){
+	tab[0][0] = 0.0;
+	tab[1][0] = 1.0;
 
 }
 void main(int argc, char** argv){
-	C = malloc(sizeof(double)*x*y);
-	*C = 2.0;
-	double D = 2.0**C;
-	printf("%f\n",D);
-	double pi = atan( 1.0 ) * 4.0;
-	printf("%f",pi);
 	int width = atoi(argv[1]);
 	int height = atoi(argv[2]);
-	double tab[width*height];
-	initialize(tab);
-	printf("%f,%f\n",tab[0],tab[1]);
+	double tab[width][height];
+	initialize(&tab[0]);
+	printf("%f,%f\n",tab[0][0],tab[1][0]);
 	exit(0);
 }
